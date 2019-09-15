fc9a209adf3c04f5be619247a0b09e5d306f6e4f
pirijuamps <pirijuamps@gmail.com>
2019-09-15 11:40:38 -0700

    words before contraction are recognized for specialCount

--- wordCounter.py
+++ wordCounter.py
@@ -1,8 +1,8 @@
-myfile = open("wordCounter/beatles.txt", "r")
+myfile = open("./beatles.txt", "r")
 data = myfile.read()
 myfile.close()
 
-words = data.split(" ")
+words = data.split()
 muchWords = len(words)
 print(data)
 print("\nwow this much words: ", muchWords)
@@ -12,8 +12,13 @@ print("\nwow this much words: ", muchWords)
 def specialCounter(specialWord, text):
     specialCounter = 0
     for x in text:
-        if x.__contains__("'"):
-            x.split("'")
+        if "'" in x:
+            x = x.split("'")
+            for y in x:
+                if y == specialWord:
+                    specialCounter+=1
+        elif x == specialWord:
+            specialCounter+=1
     return specialCounter
 
 specialWord = input()
