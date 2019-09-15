e6c338874215f34ca95cdcf31f3b70becfe1a96a
pirijuamps <pirijuamps@gmail.com>
2019-09-15 12:06:14 -0700

    now contractions can also be counted, parsed from a list in json

--- wordCounter.py
+++ wordCounter.py
@@ -1,13 +1,15 @@
+import json
+with open("./abbreviations.json", 'r') as f:
+    datastore = json.load(f)
+
 myfile = open("./beatles.txt", "r")
 data = myfile.read()
 myfile.close()
 
-words = data.split()
+words = data.lower().split()
 muchWords = len(words)
 print(data)
 print("\nwow this much words: ", muchWords)
-
-
 
 def specialCounter(specialWord, text):
     specialCounter = 0
@@ -15,8 +17,8 @@ def specialCounter(specialWord, text):
         if "'" in x:
             x = x.split("'")
             for y in x:
-                if y == specialWord:
-                    specialCounter+=1
+                if y == specialWord or y == datastore[specialWord]:
+                    specialCounter+=1 
         elif x == specialWord:
             specialCounter+=1
     return specialCounter
